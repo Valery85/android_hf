1dd06b8025f812192c510bf9c3c5adca4b1692c4
Rik <mr.valery85@yandex.ru>
2018-10-04 10:35:18 +0300

    Add method to fragment  to organize communication with MainActivity

--- Workout/app/src/main/java/com/rik/android/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/rik/android/workout/WorkoutDetailFragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutDetailFragment extends Fragment {
-
+    private long workoutId;
 
     public WorkoutDetailFragment() {
         // Required empty public constructor
@@ -23,7 +23,11 @@ public class WorkoutDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_workout_detail, container, false);
+        return view;
     }
-
+// method to transfer id in fragment from MainActivity
+    public void setWorkoutId(long workoutId) {
+        this.workoutId = workoutId;
+    }
 }
