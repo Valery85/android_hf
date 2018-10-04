68fc6d86e6e76557c49829f7e57d65410b67360d
Rik <mr.valery85@yandex.ru>
2018-10-04 11:22:46 +0300

    Transfer data in fragment and show on the screen

--- Workout/app/src/main/java/com/rik/android/workout/MainActivity.java
+++ Workout/app/src/main/java/com/rik/android/workout/MainActivity.java
@@ -3,11 +3,18 @@ package com.rik.android.workout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            // create fragment object to transfer data there
+        WorkoutDetailFragment fragment = (WorkoutDetailFragment) getFragmentManager().findFragmentById(R.id.details_fragment);
+            // for now argument hardcoded for test purpose
+        fragment.setWorkoutId(1);
+
     }
 }
--- Workout/app/src/main/java/com/rik/android/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/rik/android/workout/WorkoutDetailFragment.java
@@ -2,10 +2,12 @@ package com.rik.android.workout;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+//import android.support.v4.app.Fragment;           this is wrong library imported by default
+import android.app.Fragment;                    // right library
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,10 +25,30 @@ public class WorkoutDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_workout_detail, container, false);
-        return view;
+//        View view = inflater.inflate(R.layout.fragment_workout_detail, container, false);
+//        return view;
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
-// method to transfer id in fragment from MainActivity
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // get root object for fragment View
+        View view = getView();
+        if (view != null) {
+            // get chosen workout object
+            Workout chosenWorkout = Workout.workout[(int) workoutId];
+
+            // set data in fragment views
+            TextView nameOfWorkout = (TextView) view.findViewById(R.id.name_of_workout);
+            nameOfWorkout.setText(chosenWorkout.getName());
+            TextView descriptionOfWorkout = (TextView) view.findViewById(R.id.description_of_workout);
+            descriptionOfWorkout.setText(chosenWorkout.getDescription());
+        }
+    }
+
+    // method to transfer id in fragment from MainActivity
     public void setWorkoutId(long workoutId) {
         this.workoutId = workoutId;
     }
