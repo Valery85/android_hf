86b1c0e1b05cd83f35d30596c772e9937643cf03
Rik <mr.valery85@yandex.ru>
2018-10-05 11:14:09 +0300

    show correspondent data when item in list taped

--- Workout/app/src/main/java/com/rik/android/workout/MainActivity.java
+++ Workout/app/src/main/java/com/rik/android/workout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rik.android.workout;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -20,6 +21,16 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
     // define logic which will be used when user tap on list in WorkoutListFragment
     @Override
     public void itemClicked(long id) {
+        WorkoutDetailFragment detailsFragment = new WorkoutDetailFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        detailsFragment.setWorkoutId(id);
+        fragmentTransaction.replace(R.id.fragment_container, detailsFragment);
+
+        //to have possibility return to previously formed (tapped in list) fragment
+        fragmentTransaction.addToBackStack(null);
+        // for animation of transition between fragment changes
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
 
     }
 }
