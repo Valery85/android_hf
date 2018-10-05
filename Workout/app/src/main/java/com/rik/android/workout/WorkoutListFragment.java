c89b66056982c19b00eea469e4d72709c2d193b9
Rik <mr.valery85@yandex.ru>
2018-10-05 10:51:26 +0300

    link list fragment with activity using interface

--- Workout/app/src/main/java/com/rik/android/workout/WorkoutListFragment.java
+++ Workout/app/src/main/java/com/rik/android/workout/WorkoutListFragment.java
@@ -2,13 +2,18 @@ package com.rik.android.workout;
 
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+//import android.support.v4.app.Fragment;
+import android.app.Fragment;
+//import android.support.v4.app.ListFragment;
+import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -20,6 +25,27 @@ public class WorkoutListFragment extends ListFragment {
     public WorkoutListFragment() {
         // Required empty public constructor
     }
+
+    // to connect activity and fragment using interface
+    interface WorkoutListListener{
+        void itemClicked(long id);
+    }
+
+    private  WorkoutListListener listener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.listener = (WorkoutListListener)activity;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (listener != null){
+            listener.itemClicked(id);
+        }
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
