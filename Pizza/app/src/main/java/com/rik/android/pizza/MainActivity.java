2d5e4d069698e7723cf5d27eb5ffa19ae9f8ee5b
Rik <mr.valery85@yandex.ru>
2018-10-09 12:33:02 +0300

    Add items on the action bar

--- Pizza/app/src/main/java/com/rik/android/pizza/MainActivity.java
+++ Pizza/app/src/main/java/com/rik/android/pizza/MainActivity.java
@@ -1,10 +1,18 @@
 package com.rik.android.pizza;
 
-import android.app.Activity;
-//import android.support.v7.app.AppCompatActivity;
+//import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // fill menu, elements (items) will be added on the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
