27a7921c58b9f9039bee7e3c06b05fcb50db27c9
Rik <mr.valery85@yandex.ru>
2018-10-10 11:19:56 +0300

    add logic for create_order menu element (call OrderActivity)

--- Pizza/app/src/main/java/com/rik/android/pizza/MainActivity.java
+++ Pizza/app/src/main/java/com/rik/android/pizza/MainActivity.java
@@ -1,11 +1,31 @@
 package com.rik.android.pizza;
 
 //import android.app.Activity;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    // onOptionsItemSelected used to add logic when the element of  action bar clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                // code for create_order button
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                // code for action_settings element
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
