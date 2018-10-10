d6f3211cee6c9414e37d6a5a7fc301eb4afcd4c0
Rik <mr.valery85@yandex.ru>
2018-10-10 17:23:11 +0300

    Add action provider

--- Pizza/app/src/main/java/com/rik/android/pizza/MainActivity.java
+++ Pizza/app/src/main/java/com/rik/android/pizza/MainActivity.java
@@ -2,13 +2,35 @@ package com.rik.android.pizza;
 
 //import android.app.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+//import android.widget.ShareActionProvider;
+import android.support.v7.widget.ShareActionProvider;
 
 public class MainActivity extends AppCompatActivity {
+    private ShareActionProvider shareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // fill menu, elements (items) will be added on the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // get item of menu
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("This is my example text");
+        return super.onCreateOptionsMenu(menu);
+    }
 
     // onOptionsItemSelected used to add logic when the element of  action bar clicked
     @Override
@@ -27,16 +49,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // fill menu, elements (items) will be added on the action bar
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
