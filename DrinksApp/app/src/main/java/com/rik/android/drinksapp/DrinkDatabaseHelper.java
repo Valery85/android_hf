397a64fff843cb99f099e261616bcab7674e500f
Rik <mr.valery85@yandex.ru>
2018-10-14 20:42:25 +0300

    Add column FAVORITE in drinkDatabase DRINK table

--- DrinksApp/app/src/main/java/com/rik/android/drinksapp/DrinkDatabaseHelper.java
+++ DrinksApp/app/src/main/java/com/rik/android/drinksapp/DrinkDatabaseHelper.java
@@ -9,7 +9,7 @@ public class DrinkDatabaseHelper extends SQLiteOpenHelper {
 
     // set up name and version of database
     private static final String DB_NAME = "drinkDatabase";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     DrinkDatabaseHelper(Context context){
         super(context, DB_NAME, null, DB_VERSION);
@@ -18,20 +18,13 @@ public class DrinkDatabaseHelper extends SQLiteOpenHelper {
     // this method used to create database and data insert
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // create table DRINK with columns NAME, DESCRIPTION, IMAGE_RESOURCE_ID
-        db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "NAME TEXT," +
-                "DESCRIPTION TEXT," +
-                "IMAGE_RESOURCE_ID INTEGER); ");
-        insertDrink(db, "Latte", "Espresso with milk", R.drawable.latte );
-        insertDrink(db,"Cappuccino", "Espresso milk and foam", R.drawable.cappucino);
-        insertDrink(db, "Filter", "Some coffee", R.drawable.filter );
+        updateMyDatabase(db, 0, DB_VERSION);
     }
 
     // this method used to update database
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
     }
     // method to insert drinks in table
     private static void insertDrink(SQLiteDatabase db, String name,
@@ -44,5 +37,21 @@ public class DrinkDatabaseHelper extends SQLiteOpenHelper {
         drinkValues.put("IMAGE_RESOURCE_ID", resourceID);
         db.insert("DRINK",null, drinkValues);
 
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion){
+        if (oldVersion < 1){
+            // create table DRINK with columns NAME, DESCRIPTION, IMAGE_RESOURCE_ID
+            db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "NAME TEXT," +
+                    "DESCRIPTION TEXT," +
+                    "IMAGE_RESOURCE_ID INTEGER); ");
+            insertDrink(db, "Latte", "Espresso with milk", R.drawable.latte );
+            insertDrink(db,"Cappuccino", "Espresso milk and foam", R.drawable.cappucino);
+            insertDrink(db, "Filter", "Some coffee", R.drawable.filter );
+        }
+        if (oldVersion < 2){
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
     }
 }
